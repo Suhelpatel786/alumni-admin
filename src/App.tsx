22025c7bfdb37eb694d3ca262de8c6f24d81d45a
Suhel Patel <suhel.softcolon@gmail.com>
2024-06-20 15:32:35 +0530

    code setup , mui library setup*

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import { Box, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <Box>
+      <Typography variant="h3">Admin - Alumni</Typography>
+    </Box>
+  );
+}
+
+export default App;
